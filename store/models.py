4f6fe4baa728f04b425d3ed42525734ab6032b63
Uday Pratap <udaypratap169@gmail.com>
2021-10-16 16:15:50 +0530

    likes app created and models updated

--- store/models.py
+++ store/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.DecimalField()
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True,related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -10,6 +14,7 @@ class Product(models.Model):
     inventory = models.IntegerField
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
