cf7144c4e691505a9e581068b0d2dbb7d9c3f2bc
Uday Pratap <udaypratap169@gmail.com>
2021-10-26 12:13:10 +0530

    migration updated

--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     description = models.TextField()
     slug = models.SlugField(default="-")
     unit_price = models.DecimalField(max_digits=6,decimal_places=2)
-    inventory = models.IntegerField
+    inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
