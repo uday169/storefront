09f76708c377f9134a53f22bb9e49306a70bbc08
Uday Pratap <udaypratap169@gmail.com>
2021-10-30 17:21:03 +0530

    regiter admin for product and collection

--- store/models.py
+++ store/models.py
@@ -7,6 +7,11 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True,related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+    class Meta:
+        ordering = ['title']
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -16,6 +21,11 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self) -> str:
+        return self.title
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
