c9288160ce329b9ca24aad027f86b2a2a0269fa1
Uday Pratap <udaypratap169@gmail.com>
2021-11-09 12:05:18 +0530

    custom filter and actions

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,76 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models.query import QuerySet
 from . import models
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from django.db.models import Count
 
 
-admin.site.register(models.Collection)
-admin.site.register(models.Product)
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 20
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return "Low"
+        return "OK"
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset: QuerySet):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request, f'{updated_count} products were successfully updated.', messages.ERROR)
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 20
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = reverse('admin:store_product_changelist') + '?' + \
+            urlencode({'collection__id': str(collection.id)})
+        return format_html('<a href="{}">{}</a>', url, collection)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count('product'))
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer', 'payment_status']
+    list_editable = ['payment_status']
+    list_per_page = 20
